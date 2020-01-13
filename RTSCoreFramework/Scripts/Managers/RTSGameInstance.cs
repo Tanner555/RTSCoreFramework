5ad95e733bc5f9756dd1027d8dc8e415f56ceabf
Tanner555 <theman989898@gmail.com>
2020-01-13 06:18:51 -0800

    Added Fade Functionality To Game Instance

--- RTSCoreFramework/Scripts/Managers/RTSGameInstance.cs
+++ RTSCoreFramework/Scripts/Managers/RTSGameInstance.cs
@@ -9,6 +9,48 @@ namespace RTSCoreFramework
 {
     public class RTSGameInstance : GameInstance
     {
+        #region FieldsAndProps
+        float transitionTime = 1f;
+        string startTrigger = "Start";
+        string endTrigger = "End";
+        string FadeNameRef = "CrossFade";
+        private Animator crossFadeAnimator
+        {
+            get
+            {
+                if (_crossFadeAnimator == null)
+                {                                        
+                    _crossFadeAnimator = CrossFadeTransform.GetComponent<Animator>();
+                }
+                return _crossFadeAnimator;
+            }
+        }
+        private Animator _crossFadeAnimator = null;
+
+        private Transform CrossFadeTransform
+        {
+            get
+            {
+                //Do not call ToggleCrossFadeGameObject From Here. 
+                //Will Create Infinite Loop and Crash Unity.
+                if (_CrossFadeTransform == null)
+                {
+                    //CrossFade Should Be A Child Of This Transform
+                    foreach (Transform _child in transform)
+                    {
+                        if (_child.name.Contains(FadeNameRef))
+                        {
+                            //Found CrossFade Transform
+                            _CrossFadeTransform = _child;
+                        }
+                    }
+                }
+                return _CrossFadeTransform;
+            }
+        }
+        private Transform _CrossFadeTransform = null;
+        #endregion
+
         #region OverrideAndHideProperties
         new public static RTSGameInstance thisInstance
         {
@@ -24,5 +66,36 @@ namespace RTSCoreFramework
         }
         #endregion
 
+        #region Overrides
+        public override void LoadLevel(LevelIndex _level, ScenarioIndex _scenario)
+        {
+            StartCoroutine(FadeAndLoadLevelAfterDelay(_level, _scenario));
+        }
+
+        IEnumerator FadeAndLoadLevelAfterDelay(LevelIndex _level, ScenarioIndex _scenario)
+        {
+            ToggleCrossFadeGameObject(true);
+            crossFadeAnimator.SetTrigger(startTrigger);
+            yield return new WaitForSeconds(transitionTime);
+            LoadLevelDelay(_level, _scenario);
+            yield return new WaitForSeconds(transitionTime / 2);
+            crossFadeAnimator.SetTrigger(endTrigger);
+            yield return new WaitForSeconds(transitionTime);
+            ToggleCrossFadeGameObject(false);
+        }
+
+        private void LoadLevelDelay(LevelIndex _level, ScenarioIndex _scenario)
+        {
+            //Make Sure to Call Base Method, and Not Override
+            base.LoadLevel(_level, _scenario);
+        }
+        #endregion
+
+        #region Helpers
+        void ToggleCrossFadeGameObject(bool _enable)
+        {
+            CrossFadeTransform.gameObject.SetActive(_enable);
+        }
+        #endregion
     }
 }
