658abefced48d28cb2116eab6af11a2290902c02
Tanner555 <theman989898@gmail.com>
2020-02-26 20:55:15 -0800

    Added IAllyMovable Interface

---
+++ RTSCoreFramework/Scripts/Characters/AllyInterfaces.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RTSCoreFramework
+{
+    public interface IAllyMovable
+    {
+        void MoveAlly(Vector3 Direction, bool isFreeMoving);
+    }
+}
